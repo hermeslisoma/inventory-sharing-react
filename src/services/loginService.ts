166b48074d6aa56e421c04279bb1e6690664ee22
Eva Fann <exfann@gmail.com>
2019-06-19 09:35:17 -0400

    login finished

--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -10,14 +10,26 @@ import base64 from 'base-64'
     super("/api/user/login");
   }
 
+  getLoginHeaders = (creds) =>  {
+    // getLoginHeaders = (creds:String) =>  {
+        const headers: any = {
+      'content-type': 'application/json',
+      'Authorization': `Basic ${creds}`
+    };
+    // headers.Authorization = 'Basic '+creds;
+    console.log(headers,"this is gonna be my headers");
+ 
+    return headers;
+  }
+
 login =(username:string, password:string)=> {
     const creds = base64.encode(`${username}:${password}`)
     console.log(creds)
-    return axios.post(`${this.url}`,creds, {headers: this.getHeaders()})
+    return axios.get(`${this.url}`, {headers: this.getLoginHeaders(creds)})
                  .then(resp => {
-                   return resp.data;
+                   return resp;
                  }).catch(error => {
-                   return error.response.data
+                   return error.response
                  });
   }
 
